c3c124a03ba05c3a42af521f654e16de0626c443
Agnik Roy <102238499+Ar12agnik@users.noreply.github.com>
2025-02-04 16:52:39 +0530

    update

--- Scripts/custom/script1.js
+++ Scripts/custom/script1.js
@@ -1,23 +1,39 @@
 ﻿console.log('script1.js');
+var errors = 0
+const email_validator = /^[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,}$/
+const paswd_validator = /^(?=.{8,})(?=.*[a-zA-Z])(?=.*\d)(?=.*[!@#$%^&*(),.?":{}|<>]).*$/g
+const n_validator = /^[a-zA-Z]+( [a-zA-Z]+)*$/
+const mobileNo_ckh = /^\d{10}$/;
+const alert_con = document.getElementById("AGNIK");
 function try_login() {
-    const xhr = new XMLHttpRequest();
-    xhr.open('POST', '/dashboard/Auth', true);
-    xhr.setRequestHeader('Content-Type', 'application/json');
-    xhr.onload = function () {
-        json = JSON.parse(this.responseText);
-        if (json['code'] === 200){
-            window.location.href = `/dashboard/${json['redirect']}`
-        }
-        else {
-            console.log(this.responseText)
-        }
+    if (errors > 0) {
+        alert(`${errors} no of errors found!`)
+        return;
     }
-    xhr.send(JSON.stringify({
-        username: document.getElementById('floatingInput').value,
-        password: document.getElementById('floatingPassword').value
-    }));
+    else {
+        const xhr = new XMLHttpRequest();
+        xhr.open('POST', '/dashboard/Auth', true);
+        xhr.setRequestHeader('Content-Type', 'application/json');
+        xhr.onload = function () {
+            json = JSON.parse(this.responseText);
+            if (json['code'] === 200) {
+                window.location.href = `/dashboard/${json['redirect']}`
+            }
+            else {
+                show_alert(this.responseText["message"])
+            }
+        }
+        xhr.send(JSON.stringify({
+            username: document.getElementById('floatingInput').value,
+            password: document.getElementById('floatingPassword').value
+        }));
+    }
 }
 function try_signup() {
+    if (errors > 0) {
+        alert(`${errors}  errors found! Please Fix them to submit!!`)
+        return;
+    }
     const xhr = new XMLHttpRequest();
     xhr.open('POST', "https://localhost:44340/dashboard/createuser", true)
     xhr.setRequestHeader('Content-Type', 'application/json');
@@ -62,4 +78,97 @@ function try_signup() {
         alert("Passwords donot match")
     }
 
+}
+
+
+//validator code
+
+
+
+function show_alert(message) {
+    console.log(alert_con)
+    alert_con.classList.remove('hide');
+    alert_con.classList.add('show');
+    alert_con.innerHTML = message;
+    return false;
+}
+function hide_alert() {
+    alert_con.classList.remove('show');
+    alert_con.classList.add('hide');
+}
+function validate_email() {
+    email = document.getElementById("floatingEmail").value
+    if (!email_validator.test(email)) {
+        display_error("floatingEmail", "Invalid Email Address!!")
+    }
+    else {
+        remove_error("floatingEmail")
+    }
+}
+function validate_password1() {
+    password = document.getElementById("floatingPassword1").value
+    if (!paswd_validator.test(password)) {
+        display_error("floatingPassword1", "Password Should Contain at least one lowercase letter, uppercase letter, number, and a minimum length of 8 characters")
+    }
+    else {
+        remove_error("floatingPassword1")
+    }
+}
+function validate_phone_number() {
+    Phone = document.getElementById("floatingPhone").value
+    if (!mobileNo_ckh.test(Phone)) {
+        display_error("floatingPhone", "Invalid Phone Number")
+    }
+    else {
+        remove_error("floatingPhone")
+    }
+
+}
+function validate_password2() {
+    password = document.getElementById("floatingPassword2").value
+    if (!paswd_validator.test(password)) {
+        display_error("floatingPassword2", "Password Should Contain at least one lowercase letter, uppercase letter, number, and a minimum length of 8 characters")
+    }
+    else {
+        remove_error("floatingPassword2")
+    }
+}
+//function validate_name() {
+//    name_ = document.getElementById("name").value
+//    if (!n_validator.test(name_)) {
+//        display_error("name", "Invalid Name")
+//    } else {
+//        remove_error('name')
+//    }
+
+//}
+function display_error(id, message) {
+    errors = errors+  1
+    console.log(errors)
+    
+    element = document.getElementById(id)
+    element.classList.add("is-invalid")
+    const newelement = document.createElement("div")
+    newelement.setAttribute('id', 'validationServer04Feedback')
+    newelement.classList.add("invalid-feedback")
+    newelement.textContent = message
+    element.parentNode.appendChild(newelement)
+}
+function remove_error(id) {
+    try {
+        errors =errors - 1
+
+        let element = document.getElementById(id);
+        if (!element) throw new Error("Element not found");
+
+        element.classList.remove("is-invalid");
+        element.classList.add("is-valid");
+
+        let errorElement = document.getElementById(id + "-feedback");
+        if (errorElement) {
+            errorElement.remove();
+        }
+    } catch (error) {
+        console.error("remove_error: ", error.message);
+    }
 }
